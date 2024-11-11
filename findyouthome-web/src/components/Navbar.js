fb7677eebd523b41aaf20b06b3da732169a0735b
Ahmad Mcheik <ahmadmsheik219@gmail.com>
2024-11-11 22:22:38 +0200

    Adding Authentication

--- findyouthome-web/src/components/Navbar.js
+++ findyouthome-web/src/components/Navbar.js
@@ -1,18 +1,40 @@
-// Navbar.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const linkStyle = {
+    color: 'blue',
+    textDecoration: 'none',
+    padding: '8px',
+  };
+
+   const navigate = useNavigate();
+
+  const handleSignIn = () => {
+    navigate('/login');
+  };
+
+
   return (
-    <nav>
-      <ul style={{ display: 'flex', listStyle: 'none', gap: '1rem' }}>
+    <nav style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
+      <ul style={{ display: 'flex', listStyle: 'none', gap: '1rem', margin: 0, padding: 0 }}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" style={linkStyle}>Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" style={linkStyle}>About</Link>
+        </li>
+        <li>
+          <Link to="/services" style={linkStyle}>Services</Link>
+        </li>
+        <li>
+          <Link to="/favorites" style={linkStyle}>Favorite</Link>
         </li>
       </ul>
+      
+      <button onClick={handleSignIn} style={{ padding: '8px 16px', backgroundColor: 'blue', color: 'white', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
+        Sign In
+      </button>
     </nav>
   );
 };
--- findyouthome-web/src/pages/App.js
+++ findyouthome-web/src/pages/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Home from './Home';
 import About from './About';
+import Services from './Services';
+import Favorites from './favorites';
+import Login from './Login';
+import Register from './Register'
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
